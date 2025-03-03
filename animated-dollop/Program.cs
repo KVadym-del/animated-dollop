3dda432f575288dc6649d8fa894b9a66c89b9dd1
Vadym Kozachuk <77024298+KVadym-del@users.noreply.github.com>
2025-03-03 12:11:32 +0100

    Modification 3

--- animated-dollop/Program.cs
+++ animated-dollop/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System.Numerics;
 
 BigInteger add(BigInteger a, BigInteger b) => a + b;
-BigInteger mult(BigInteger a, BigInteger b) => a * b;
+BigInteger multiply(BigInteger a, BigInteger b) => a * b;
+double division(double a, double b) => a / b;
 
 Console.WriteLine("Hello, World!");
 Console.WriteLine(add(12, 3));
-Console.WriteLine(mult(12, 3));
+Console.WriteLine(multiply(12, 3));
+Console.WriteLine(division(12, 3));
